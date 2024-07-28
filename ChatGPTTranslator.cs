ee8e77d96659f2caac4436851a5cca53ab3dfc16
Caio Dario <chnrqpd@gmail.com>
2024-07-28 01:24:58 -0300

    refactor: changing system context logic

--- ChatGPTTranslator.cs
+++ ChatGPTTranslator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -13,6 +14,11 @@ namespace Echoglossian
     private readonly IPluginLog pluginLog;
     private readonly string apiKey;
     private readonly HttpClient httpClient;
+    private readonly object systemMessage = new
+    {
+      role = "system",
+      content = "You are a professional translator and localizer. Translate the following text to {targetLanguage} in a natural and fluent manner, making it feel as if it were originally written in that language. Avoid literal translations and ensure the translation is localized, preserving the context, meaning, emotion, tone, and logical and syntactical sense for the target language. Use your knowledge of the Final Fantasy XIV universe to correctly translate names of cities, characters, locations, items, and use the appropriate pronouns based on the original text. Translate text inside <> and return the translated text inside <> as well. Only return in <> the text that was already inside the brackets within <> and not the entire text. Ensure that the translated text does not exceed 400 characters. If the translated text exceeds this limit, reduce it while maintaining the sense and context. Translate with attention to the context, sentiment, and tone of the original phrase to create a vivid and accurate translation."
+    };
 
     public ChatGPTTranslator(IPluginLog pluginLog, string apiKey)
     {
@@ -35,25 +41,19 @@ namespace Echoglossian
 
       try
       {
+
+        var systemContent = ((dynamic)systemMessage).content.Replace("{targetLanguage}", FormatTargetLanguage(targetLanguage));
+        this.pluginLog.Warning($"{systemContent}");
+        var messages = new List<Object>
+        {
+          new { role = "system", content = systemContent },
+          new { role = "user", content = text }
+        };
+
         var requestBody = new
         {
           model = "gpt-3.5-turbo",
-          messages = new[]
-            {
-              new
-               {
-                role = "system",
-                content = $"You are a professional translator and localizer. Translate the following text to {targetLanguage} in a natural and fluent manner, making it feel as if it were originally written in that language. Avoid literal translations and ensure the translation is localized, preserving the context, meaning, emotion, tone, and logical and syntactical sense for the target language. " +
-                          $"Use your knowledge of the Final Fantasy XIV universe to correctly translate names of cities, characters, locations, items, and use the appropriate pronouns based on the original text. " +
-                          $"Translate text inside <> and return the translated text inside <> as well. Only return in <> the text that was already inside the brackets within <> and not the entire text. " +
-                          $"Ensure that the translated text does not exceed 400 characters. If the translated text exceeds this limit, reduce it while maintaining the sense and context. " +
-                          $"Translate with attention to the context, sentiment, and tone of the original phrase to create a vivid and accurate translation." },
-          new
-              {
-                role = "user",
-                content = text
-              }
-            }
+          messages = messages
         };
 
         var requestBodyText = JsonConvert.SerializeObject(requestBody);
@@ -87,6 +87,19 @@ namespace Echoglossian
     public string Translate(string text, string sourceLanguage, string targetLanguage)
     {
       return this.TranslateAsync(text, sourceLanguage, targetLanguage).Result;
+    }
+
+    private string FormatTargetLanguage(string source)
+    {
+      switch (source)
+      {
+        case "es":
+          return "Spanish";
+        case "pt":
+          return "Brazilian Portuguese";
+        default:
+          return source.ToUpper();
+      }
     }
   }
 
